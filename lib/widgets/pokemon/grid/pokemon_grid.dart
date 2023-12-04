d93fb29381513fe1adbfceab425048ffa8134ab8
odreman <43256500+odreman@users.noreply.github.com>
2023-12-03 21:41:41 -0300

    Paginados de pokemons

--- lib/widgets/pokemon/grid/pokemon_grid.dart
+++ lib/widgets/pokemon/grid/pokemon_grid.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:pokedex_ofd2/models/pokemon_details.dart';
 import 'package:pokedex_ofd2/widgets/pokemon/grid/pokemon_grid_card.dart';
@@ -22,6 +24,9 @@ class PokemonGrid extends StatefulWidget {
 
 class _PokemonsGridState extends State<PokemonGrid> {
   final ScrollController scrollController = ScrollController();
+  final StreamController<List<PokemonDetails>> streamController =
+      StreamController<List<PokemonDetails>>();
+  List<PokemonDetails> pokemonsListWithDetails = [];
 
   @override
   void initState() {
@@ -36,51 +41,87 @@ class _PokemonsGridState extends State<PokemonGrid> {
   }
 
   @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(
-      controller: scrollController,
-      slivers: [
-        const PokemonScreenAppBar(),
-        SliverPadding(
+      body: CustomScrollView(
+        controller: scrollController,
+        slivers: [
+          const PokemonScreenAppBar(),
+          SliverPadding(
             padding: const EdgeInsets.all(8),
-            sliver: (SliverGrid(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                  childAspectRatio: 1),
-              delegate: SliverChildBuilderDelegate(
-                childCount: widget.pokemons.length + 1,
-                (context, index) {
-                  if (index < widget.pokemons.length) {
-                    final PokemonDetails pokemons = widget.pokemons[index];
-                    return PokemonGridCard(
-                      pokemons: pokemons,
-                      onPress: () => Navigator.pushNamed(
-                          context, 'detailsPokemon',
-                          arguments: pokemons),
-                    );
-                  } else if (index == widget.maxItems) {
-                    return const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                      child: Center(
-                        child: Text('No more items'),
-                      ),
-                    );
-                  } else {
-                    return const Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                  }
-                },
-              ),
-            )))
-      ],
-    ));
+            sliver: StreamBuilder<List<PokemonDetails>>(
+              stream: streamController.stream,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return SliverGrid(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                      childAspectRatio: 1,
+                    ),
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        if (index < snapshot.data!.length) {
+                          final PokemonDetails pokemons = snapshot.data![index];
+                          return PokemonGridCard(
+                            pokemons: pokemons,
+                            onPress: () => Navigator.pushNamed(
+                              context,
+                              'detailsPokemon',
+                              arguments: pokemons,
+                            ),
+                          );
+                        } else if (index == widget.maxItems) {
+                          return const Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 5,
+                              vertical: 5,
+                            ),
+                            child: Center(
+                              child: Text('No more items'),
+                            ),
+                          );
+                        } else {
+                          return const Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 20,
+                            ),
+                            child: Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          );
+                        }
+                      },
+                      childCount: snapshot.data!.length + 1,
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return SliverFillRemaining(
+                    child: Center(
+                      child: Text('Error: ${snapshot.error}'),
+                    ),
+                  );
+                } else {
+                  return SliverFillRemaining(
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
